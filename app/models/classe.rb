7fda01ad8129d0687b295a1c6a1277f0979ac14f
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-11-16 14:59:22 +0300

    create teachers migration

--- app/models/classe.rb
+++ app/models/classe.rb
@@ -1,4 +1,5 @@
 class Classe < ActiveRecord::Base
+  has_one :teacher
   has_many :enrolls
   has_many :students, through: :enrolls
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_16_080147) do
+ActiveRecord::Schema.define(version: 2022_11_16_114534) do
 
   create_table "classes", force: :cascade do |t|
     t.string "name"
@@ -25,6 +25,13 @@ ActiveRecord::Schema.define(version: 2022_11_16_080147) do
 
   create_table "students", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "teachers", force: :cascade do |t|
+    t.string "name"
+    t.integer "class_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
